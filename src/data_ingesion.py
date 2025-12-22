7a386117cd2d75a0b664035ec085828017de29b5
Ankit <ankitkag8823@gmail.com>
2025-12-22 17:26:54 +0530

    pipeline ready with all components

--- src/data_ingesion.py
+++ src/data_ingesion.py
@@ -48,7 +48,7 @@ def load_params(params_path: str) -> dict:
 def load_data(data_url: str) -> pd.DataFrame:
     """Load data from a CSV file."""
     try:
-        df = pd.read_csv(data_url)
+        df = pd.read_csv(data_url,encoding="latin1")
         logger.debug('Data loaded from %s', data_url)
         return df
     except pd.errors.ParserError as e:
@@ -86,10 +86,9 @@ def save_data(train_data: pd.DataFrame, test_data: pd.DataFrame, data_path: str)
 
 def main():
     try:
-        params = load_params(params_path='params.yaml')
-        test_size = params['data_ingestion']['test_size']
-        # test_size = 0.2
-        data_path = 'https://raw.githubusercontent.com/vikashishere/Datasets/main/spam.csv'
+        
+        test_size = 0.2
+        data_path = "https://github.com/Ankit8823/datasets/raw/refs/heads/main/spam.csv"
         df = load_data(data_url=data_path)
         final_df = preprocess_data(df)
         train_data, test_data = train_test_split(final_df, test_size=test_size, random_state=2)
